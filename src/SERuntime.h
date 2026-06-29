0690bea31e511af9724b903c2761a627a56f03b8
Ilan Vinograd <ilanv1111@gmail.com>
2026-06-29 23:23:53 +0300

    added all base files for core engine

---
+++ src/SERuntime.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#if defined(_WIN32) || defined(_WIN64)
+
+    #ifdef SERUNTIME_EXPORTS
+        #define SERUNTIME_API __declspec(dllexport)
+    #else
+        #define SERUNTIME_API __declspec(dllimport)
+    #endif
+
+#else
+
+    #ifdef SERUNTIME_EXPORTS
+        #define SERUNTIME_API __attribute__((visibility("default")))
+    #else
+        #define SERUNTIME_API
+    #endif
+    
+#endif
+
+extern "C" SERUNTIME_API int Add(int a, int b);
